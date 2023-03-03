7f346a923d9f2873b9f3889415d23de15f4fa700
Alejandro Cortes <acr20020221@gmail.com>
2023-03-02 22:03:21 -0500

    download

--- assets/JS/responsive.js
+++ assets/JS/responsive.js
@@ -1,7 +1,12 @@
 const link = document.querySelectorAll(".navegacion__apartado__link");
 const lista = document.querySelector("#check");
+const boton = document.querySelector("[data-download]");
 link.forEach((element) => {
   element.addEventListener("click", () => {
     lista.checked = false;
   });
+});
+
+boton.addEventListener("click", () => {
+  lista.checked = false;
 });
